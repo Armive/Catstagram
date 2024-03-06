9021d1b8cabb95fdebf6c196ab90e3eb98484e35
Daniel fernando reyes v <dfreyes1236@gmail.com>
2024-03-06 12:29:06 -0500

    Upload sign up ui 💅

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,150 +1,185 @@
 "use client";
+import { z } from "zod";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import {
-  BirthdayIcon,
-  GithubIcon,
-  LeftArrow,
-  RightArrow,
+	BirthdayIcon,
+	EmailIcon,
+	GithubIcon,
+	LeftArrow,
+	PasswordIcon,
+	RightArrow,
 } from "@/components/icons";
 import Image from "next/image";
 import { Label } from "@/components/ui/label";
 import { useState } from "react";
+import {
+	Card,
+	CardContent,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from "@/components/ui/card";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "@/components/ui/select";
 
 export default function SignUp() {
-  const [view, setView] = useState(0);
-  return view === 0 ? (
-    <div className="flex min-h-screen items-center justify-center ">
-      <div className="w-full max-w-xs p-8 gap-4 space-y-6 rounded-lg shadow-md flex flex-col">
-        <Image
-          src="/catstagram.png"
-          alt="catslogo"
-          className=" hidden xl:flex dark:invert self-center"
-          width={159}
-          height={38}
-        />
-        <form
-          className="flex gap-5 flex-col"
-          onSubmit={(e) => {
-            e.preventDefault();
-            setView(1);
-          }}
-        >
-          <Label htmlFor="email" className="flex gap-3 flex-col">
-            <span>Your Email</span>
-            <Input
-              placeholder="Example: johndoe@gmail.com"
-              type="text"
-              id="email"
-            />
-          </Label>
+	const [view, setView] = useState(0);
+	const newLocal = "";
+	const onSubmit = (e: any) => {
+		e.preventDefault();
+		const formData = new FormData(e.target);
+		setView((v) => v + 1);
+	};
 
-          <Label htmlFor="name" className="flex gap-3 flex-col">
-            <span>Your Name</span>
-            <Input placeholder="Example: John Doe" type="text" id="name" />
-          </Label>
+	return (
+		<main className="flex justify-center">
+			{view === 0 && (
+				<Card className="w-[350px] ">
+					<CardHeader className="flex items-center">
+						<Image
+							src="/catstagram.png"
+							alt="catslogo"
+							className=" hidden xl:flex dark:invert self-center"
+							width={159}
+							height={38}
+						/>
+						<CardTitle>Create Your Account</CardTitle>
+						<PasswordIcon />
+					</CardHeader>
+					<CardContent>
+						<form onSubmit={onSubmit}>
+							<div className="grid w-full items-center gap-4">
+								<div className="flex flex-col space-y-1.5">
+									<Label htmlFor="name">Your Name</Label>
+									<Input id="Name" name="name" placeholder="Tomy.cat" />
+								</div>
+								<div className="flex flex-col space-y-1.5">
+									<Label htmlFor="email">Your Email</Label>
+									<Input
+										type="email"
+										id="email"
+										placeholder="johndoe@gmail.com"
+									/>
+								</div>
+								<div className="flex flex-col space-y-1.5">
+									<Label htmlFor="password">Your Password</Label>
+									<Input type="password" id="password" placeholder="*******" />
+								</div>
+							</div>
+							<section className="flex justify-center items-center gap-3 mt-4">
+								<Button type="submit" className="mt-4">
+									<span>Go Back</span>
+									<LeftArrow />
+								</Button>
+								<Button type="submit" className="mt-4">
+									<span>Next step</span>
+									<RightArrow />
+								</Button>
+							</section>
+						</form>
+					</CardContent>
+				</Card>
+			)}
+			{view === 1 && (
+				<Card className="w-[350px]">
+					<CardHeader className="flex items-center">
+						<Image
+							src="/catstagram.png"
+							alt="catslogo"
+							className=" hidden xl:flex dark:invert self-center"
+							width={159}
+							height={38}
+						/>
 
-          <Label htmlFor="username" className="flex gap-3 flex-col">
-            <span>Your Username</span>
-            <Input placeholder="Example: Tomy.cat" type="text" id="username" />
-          </Label>
+						<CardTitle>Date of birth and gender</CardTitle>
+						<BirthdayIcon />
+					</CardHeader>
+					<CardContent>
+						<form onSubmit={onSubmit} className="flex gap-4 flex-col">
+							<section className="flex w-full items-center justify-center gap-4">
+								<div className="flex flex-col space-y-1.5 items-center">
+									<Label htmlFor="day">Day</Label>
+									<Input id="day" name="day" placeholder="8" />
+								</div>
+								<div className="flex flex-col space-y-1.5 items-center">
+									<Label htmlFor="month">Month</Label>
+									<Input id="month" name="month" placeholder="11" />
+								</div>
+								<div className="flex flex-col space-y-1.5 items-center">
+									<Label htmlFor="year">Year</Label>
+									<Input id="year" name="year" placeholder="2021" />
+								</div>
+							</section>
 
-          <Label htmlFor="password" className="flex gap-3 flex-col">
-            <span>Your Password</span>
-            <Input
-              placeholder="Example: tomycat124"
-              type="text"
-              id="password"
-            />
-          </Label>
-
-          <Button className="w-full" type="submit">
-            Next step
-            <RightArrow />
-          </Button>
-        </form>
-        <div className="flex items-center">
-          <div className="flex-grow border-t " />
-          <span className="mx-4 text-sm ">OR</span>
-          <div className="flex-grow border-t " />
-        </div>
-        <div className="text-center">
-          <Button className="flex items-center justify-center space-x-2 w-full">
-            <GithubIcon />
-            <span>Sign up with Github</span>
-          </Button>
-        </div>
-        <div className="text-center">
-          <span className="text-sm">Have an account? </span>
-          <Link className="text-sm" href="/login">
-            Log in
-          </Link>
-        </div>
-      </div>
-    </div>
-  ) : (
-    <div>
-      <form
-        action=""
-        className="flex flex-col min-h-screen items-center justify-center gap-3"
-      >
-        <Image
-          src="/catstagram.png"
-          alt="catslogo"
-          className=" hidden xl:flex dark:invert self-center"
-          width={159}
-          height={38}
-        />
-        <BirthdayIcon />
-
-        <Label className="flex gap-3 flex-col ">
-          <span className="self-center">Your Birthday</span>
-          <div className="flex gap-2">
-            <Label htmlFor="day" className="flex items-center flex-col gap-1">
-              <span>Day</span>
-              <Input
-                placeholder="1"
-                type="text"
-                id="day"
-                className="w-16 flex justify-center items-center"
-              />
-            </Label>
-            <Label htmlFor="month" className="flex items-center flex-col gap-1">
-              <span>Month</span>
-              <Input
-                placeholder="12"
-                type="text"
-                id="month"
-                className="w-16 flex justify-center items-center"
-              />
-            </Label>
-            <Label htmlFor="year" className="flex items-center flex-col gap-1">
-              <span>Year</span>
-              <Input
-                placeholder="11"
-                type="text"
-                id="year"
-                className="w-16 flex justify-center items-center"
-              />
-            </Label>
-          </div>
-        </Label>
-        <Button
-          type="submit"
-          className="bg-white text-black rounded-lg font-semibold"
-        >
-          Next Step
-          <RightArrow />
-        </Button>
-        <Button
-          type="button"
-          className="bg-white text-black rounded-lg font-semibold"
-        >
-          Go Back
-          <LeftArrow />
-        </Button>
-      </form>
-    </div>
-  );
+							<section className="flex items-center gap-3 flex-col">
+								<Label htmlFor="gender">Gender</Label>
+								<Select>
+									<SelectTrigger id="gender">
+										<SelectValue placeholder="Select your gender" />
+									</SelectTrigger>
+									<SelectContent position="popper">
+										<SelectItem value="female">Female</SelectItem>
+										<SelectItem value="male">Male</SelectItem>
+										<SelectItem value="none">Better not to say</SelectItem>
+									</SelectContent>
+								</Select>
+							</section>
+							<section className="flex justify-center items-center gap-3 mt-4">
+								<Button type="submit" className="mt-4">
+									<span>Go Back</span>
+									<LeftArrow />
+								</Button>
+								<Button type="submit" className="mt-4">
+									<span>Next step</span>
+									<RightArrow />
+								</Button>
+							</section>
+						</form>
+					</CardContent>
+				</Card>
+			)}
+			{view === 2 && (
+				<Card className="w-[350px]">
+					<CardHeader className="flex items-center">
+						<Image
+							src="/catstagram.png"
+							alt="catslogo"
+							className=" hidden xl:flex dark:invert self-center"
+							width={159}
+							height={38}
+						/>
+						<CardTitle>Verify your email</CardTitle>
+						<EmailIcon />
+					</CardHeader>
+					<CardContent className="flex justify-center flex-col">
+						<p className="text-center">
+							To enter to Catstagram go to your Email to verify the Email
+							address.
+						</p>
+						<section className="flex justify-center items-center gap-3 mt-4">
+							<Button type="submit">
+								<LeftArrow />
+								<span>Go Back</span>
+							</Button>
+							<a
+								href="http://"
+								target="_blank"
+								rel="noopener noreferrer"
+								className="text-black/80 hover:text-gray-500 dark:text-white duration-200 text-center"
+							>
+								Click to confirm Email
+							</a>
+						</section>
+					</CardContent>
+				</Card>
+			)}
+		</main>
+	);
 }

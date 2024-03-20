5f964421a70dbdabdde15692a0144d81693246da
Daniel fernando reyes v <dfreyes1236@gmail.com>
2024-03-19 19:19:47 -0500

    Upload Sign up progress bar componet 🗂️

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Progress } from "@/components/ui/progress";
+
 import { SignUpContext } from "@/components/signUpContext";
 import { z } from "zod";
 import { useContext, useEffect, useState } from "react";
@@ -31,170 +33,183 @@ import {
 } from "@/components/ui/select";
 
 export default function SignUp() {
-	const {setUserData, userData} = useContext(SignUpContext)
+	const { setUserData, userData } = useContext(SignUpContext);
 	const [view, setView] = useState(0);
 	const onSubmit = (e: any) => {
 		e.preventDefault();
 		const formData = new FormData(e.target);
 		setView((v) => v + 1);
-		const parsedFormData =Object.fromEntries(formData.entries())
-		setUserData((data:SignUpData)=>({...data,...parsedFormData}))
-
+		const parsedFormData = Object.fromEntries(formData.entries());
+		setUserData((data: SignUpData) => ({ ...data, ...parsedFormData }));
 	};
-	useEffect(()=>{
-		console.log(userData)
-	
-	}, [userData])
-	const onGoBack = ()=>{
-		setView((view)=>view-1)
-	}
+	useEffect(() => {
+		console.log(userData);
+	}, [userData]);
+	const onGoBack = () => {
+		setView((view) => view - 1);
+	};
 	return (
-		<main className="flex justify-center">
-			{view === 0 && (
-				<Card className="w-[350px] ">
-					<CardHeader className="flex items-center">
-						<Image
-							src="/catstagram.png"
-							alt="catslogo"
-							className=" hidden xl:flex dark:invert self-center"
-							width={159}
-							height={38}
-						/>
-						<CardTitle>Create Your Account</CardTitle>
-						<PasswordIcon />
-					</CardHeader>
-					<CardContent>
-						<form onSubmit={onSubmit}>
-							<div className="grid w-full items-center gap-4">
-								<div className="flex flex-col space-y-1.5">
-									<Label htmlFor="name">Your Name</Label>
-									<Input id="Name" name="name" placeholder="Tomy.cat" required/>
-								</div>
-								<div className="flex flex-col space-y-1.5">
-									<Label htmlFor="email">Your Email</Label>
-									<Input
-										type="email"
-										id="email"
-										placeholder="johndoe@gmail.com"
-										name='email'
-										required
-									/>
-								</div>
-								<div className="flex flex-col space-y-1.5 w-full">
-									<Label htmlFor="password">Your Password</Label>
-									<Input type="password" id="password" placeholder="*******" name="password" required/>
-								</div>
-							</div>
-							<Button type="submit" className="mt-4 w-full">
-								<span>Next step</span>
-								<RightArrow />
-							</Button>
-						</form>
-						<div className="flex items-center my-4">
-							<div className="flex-grow border-t " />
-							<span className="mx-4 text-sm ">OR</span>
-							<div className="flex-grow border-t " />
-						</div>
-						<Button className="flex items-center justify-center space-x-2 w-full">
-							<GithubIcon />
-							<span>Log in with Github</span>
-						</Button>
-					</CardContent>
-				</Card>
-			)}
-			{view === 1 && (
-				<Card className="w-[350px]">
-					<CardHeader className="flex items-center">
-						<Image
-							src="/catstagram.png"
-							alt="catslogo"
-							className=" hidden xl:flex dark:invert self-center"
-							width={159}
-							height={38}
-						/>
+		<main className="flex justify-center ">
+			<Card className="w-[350px] p-6">
+				<Progress value={33.3 * (view + 1)} />
 
-						<CardTitle>Date of birth and gender</CardTitle>
-						<BirthdayIcon />
-					</CardHeader>
-					<CardContent>
-						<form onSubmit={onSubmit} className="flex gap-4 flex-col">
-							<section className="flex w-full items-center justify-center gap-4">
-								<div className="flex flex-col space-y-1.5 items-center">
-									<Label htmlFor="day">Day</Label>
-									<Input id="day" name="day" placeholder="8"  required/>
+				{view === 0 && (
+					<>
+						<CardHeader className="flex items-center">
+							<Image
+								src="/catstagram.png"
+								alt="catslogo"
+								className=" hidden xl:flex dark:invert self-center"
+								width={159}
+								height={38}
+							/>
+							<CardTitle>Create Your Account</CardTitle>
+							<PasswordIcon />
+						</CardHeader>
+						<CardContent>
+							<form onSubmit={onSubmit}>
+								<div className="grid w-full items-center gap-4">
+									<div className="flex flex-col space-y-1.5">
+										<Label htmlFor="name">Your Name</Label>
+										<Input
+											id="Name"
+											name="name"
+											placeholder="Tomy.cat"
+											required
+										/>
+									</div>
+									<div className="flex flex-col space-y-1.5">
+										<Label htmlFor="email">Your Email</Label>
+										<Input
+											type="email"
+											id="email"
+											placeholder="johndoe@gmail.com"
+											name="email"
+											required
+										/>
+									</div>
+									<div className="flex flex-col space-y-1.5 w-full">
+										<Label htmlFor="password">Your Password</Label>
+										<Input
+											type="password"
+											id="password"
+											placeholder="*******"
+											name="password"
+											required
+										/>
+									</div>
 								</div>
-								<div className="flex flex-col space-y-1.5 items-center">
-									<Label htmlFor="month">Month</Label>
-									<Input id="month" name="month" placeholder="11" required/>
-								</div>
-								<div className="flex flex-col space-y-1.5 items-center">
-									<Label htmlFor="year">Year</Label>
-									<Input id="year" name="year" placeholder="2021" required />
-								</div>
-							</section>
-
-							<section className="flex items-center gap-3 flex-col">
-								<Label htmlFor="gender">Gender</Label>
-								<Select name='gender' required>
-									<SelectTrigger id="gender">
-										<SelectValue placeholder="Select your gender" />
-									</SelectTrigger>
-									<SelectContent position="popper">
-										<SelectItem value="female">Female</SelectItem>
-										<SelectItem value="male">Male</SelectItem>
-										<SelectItem value="none">Better not to say</SelectItem>
-									</SelectContent>
-								</Select>
-							</section>
-							<section className="flex justify-center items-center gap-3 mt-4">
-								<Button type="button" className="mt-4" onClick={onGoBack}>
-									<span>Go Back</span>
-									<LeftArrow />
-								</Button>
-								<Button type="submit" className="mt-4">
+								<Button type="submit" className="mt-4 w-full">
 									<span>Next step</span>
 									<RightArrow />
 								</Button>
-							</section>
-						</form>
-					</CardContent>
-				</Card>
-			)}
-			{view === 2 && (
-				<Card className="w-[350px]">
-					<CardHeader className="flex items-center">
-						<Image
-							src="/catstagram.png"
-							alt="catslogo"
-							className=" hidden xl:flex dark:invert self-center"
-							width={159}
-							height={38}
-						/>
-						<CardTitle>Verify your email</CardTitle>
-						<EmailIcon />
-					</CardHeader>
-					<CardContent className="flex justify-center flex-col">
-						<p className="text-center">
-							To enter to Catstagram go to your Email to verify the Email
-							address.
-						</p>
-						<section className="flex justify-center items-center gap-3 mt-4">
-							<Button type="button">
-								<LeftArrow />
-								<span>Go Back</span>
+							</form>
+							<div className="flex items-center my-4">
+								<div className="flex-grow border-t " />
+								<span className="mx-4 text-sm ">OR</span>
+								<div className="flex-grow border-t " />
+							</div>
+							<Button className="flex items-center justify-center space-x-2 w-full">
+								<GithubIcon />
+								<span>Log in with Github</span>
 							</Button>
-							<a
-								href="http://"
-								target="_blank"
-								rel="noopener noreferrer"
-								className="text-black/80 hover:text-gray-500 dark:text-white duration-200 text-center"
-							>
-								Click to confirm Email
-							</a>
-						</section>
-					</CardContent>
-				</Card>
-			)}
+						</CardContent>
+					</>
+				)}
+				{view === 1 && (
+					<>
+						<CardHeader className="flex items-center">
+							<Image
+								src="/catstagram.png"
+								alt="catslogo"
+								className=" hidden xl:flex dark:invert self-center"
+								width={159}
+								height={38}
+							/>
+
+							<CardTitle>Date of birth and gender</CardTitle>
+							<BirthdayIcon />
+						</CardHeader>
+						<CardContent>
+							<form onSubmit={onSubmit} className="flex gap-4 flex-col">
+								<section className="flex w-full items-center justify-center gap-4">
+									<div className="flex flex-col space-y-1.5 items-center">
+										<Label htmlFor="day">Day</Label>
+										<Input id="day" name="day" placeholder="8" required />
+									</div>
+									<div className="flex flex-col space-y-1.5 items-center">
+										<Label htmlFor="month">Month</Label>
+										<Input id="month" name="month" placeholder="11" required />
+									</div>
+									<div className="flex flex-col space-y-1.5 items-center">
+										<Label htmlFor="year">Year</Label>
+										<Input id="year" name="year" placeholder="2021" required />
+									</div>
+								</section>
+
+								<section className="flex items-center gap-3 flex-col">
+									<Label htmlFor="gender">Gender</Label>
+									<Select name="gender" required>
+										<SelectTrigger id="gender">
+											<SelectValue placeholder="Select your gender" />
+										</SelectTrigger>
+										<SelectContent position="popper">
+											<SelectItem value="female">Female</SelectItem>
+											<SelectItem value="male">Male</SelectItem>
+											<SelectItem value="none">Better not to say</SelectItem>
+										</SelectContent>
+									</Select>
+								</section>
+								<section className="flex justify-center items-center gap-3 mt-4">
+									<Button type="button" className="mt-4" onClick={onGoBack}>
+										<span>Go Back</span>
+										<LeftArrow />
+									</Button>
+									<Button type="submit" className="mt-4">
+										<span>Next step</span>
+										<RightArrow />
+									</Button>
+								</section>
+							</form>
+						</CardContent>
+					</>
+				)}
+				{view === 2 && (
+					<>
+						<CardHeader className="flex items-center">
+							<Image
+								src="/catstagram.png"
+								alt="catslogo"
+								className=" hidden xl:flex dark:invert self-center"
+								width={159}
+								height={38}
+							/>
+							<CardTitle>Verify your email</CardTitle>
+							<EmailIcon />
+						</CardHeader>
+						<CardContent className="flex justify-center flex-col">
+							<p className="text-center">
+								To enter to Catstagram go to your Email to verify the Email
+								address.
+							</p>
+							<section className="flex justify-center items-center gap-3 mt-4">
+								<Button type="button">
+									<LeftArrow />
+									<span>Go Back</span>
+								</Button>
+								<a
+									href="http://"
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-black/80 hover:text-gray-500 dark:text-white duration-200 text-center"
+								>
+									Click to confirm Email
+								</a>
+							</section>
+						</CardContent>
+					</>
+				)}
+			</Card>
 		</main>
 	);
 }

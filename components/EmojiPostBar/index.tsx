6eb55cff918f50fa70dee8f0aa430879b01bdbcf
Daniel fernando reyes v <dfreyes1236@gmail.com>
2024-05-15 19:08:38 -0500

    Upload emoji bar

---
+++ components/EmojiPostBar/index.tsx
@@ -0,0 +1,55 @@
+import {
+  SmileIcon,
+  FaceHoldingBackTearsIcon,
+  AngryFaceIcon,
+  SadFaceIcon,
+  LoveFaceIcon,
+} from "../icons";
+import { Button } from "../ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
+
+export function EmojiPostBar() {
+  return (
+    <div>
+      <TooltipProvider>
+        <Tooltip>
+          <TooltipTrigger>
+            <SmileIcon />
+          </TooltipTrigger>
+
+          <TooltipContent className="flex gap-1 justify-center align-middle">
+            <Button
+              variant="outline"
+              className="p-1 flex items-center justify-center"
+            >
+              <FaceHoldingBackTearsIcon />
+            </Button>
+            <Button
+              variant="outline"
+              className="p-1 flex items-center justify-center"
+            >
+              <AngryFaceIcon />
+            </Button>
+            <Button
+              variant="outline"
+              className="p-1 flex items-center justify-center"
+            >
+              <SadFaceIcon />
+            </Button>
+            <Button
+              variant="outline"
+              className="p-1 flex items-center justify-center"
+            >
+              <LoveFaceIcon />
+            </Button>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
+    </div>
+  );
+}

85442af2ee10da275d313ee51370cfd725104823
Daniel fernando reyes v <dfreyes1236@gmail.com>
2024-06-01 12:56:31 -0500

    Fix icon issue  ✖️

--- components/icons/index.tsx
+++ components/icons/index.tsx
@@ -321,21 +321,7 @@ function PeopleIcon() {
   );
 }
 
-function StarIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-    >
-      <path
-        fill="currentColor"
-        d="m8.85 17.825l3.15-1.9l3.15 1.925l-.825-3.6l2.775-2.4l-3.65-.325l-1.45-3.4l-1.45 3.375l-3.65.325l2.775 2.425zm-1.525 2.098l1.24-5.313l-4.123-3.572l5.431-.47L12 5.557l2.127 5.01l5.43.47l-4.122 3.572l1.24 5.313L12 17.102zM12 13.25"
-      />
-    </svg>
-  );
-}
+
 
 
 

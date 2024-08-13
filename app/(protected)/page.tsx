904d335b8231c2c64dfb5e606eb4c78c09324809
Juan <jsreyes979@gmail.com>
2024-08-13 11:49:13 -0500

    Optimize feed posts

--- app/(protected)/page.tsx
+++ app/(protected)/page.tsx
@@ -2,9 +2,17 @@ import { Post } from "@/components/Post";
 import { createClient } from "@/utils/supabase/server";
 export default async function Home() {
 	const supabase = createClient();
-	const { data: posts } = await supabase
+	const { data: posts, error } = await supabase
 		.from("posts")
-		.select("*")
+		.select(`*,
+			profiles(name, avatar_url),
+			post_likes(user_id),
+			saved_posts(user_id),
+			comments(*
+			,
+			profiles(name, avatar_url)
+			)
+			`)
 		.order("created_at", {
 			ascending: false,
 		});
@@ -14,28 +22,15 @@ export default async function Home() {
 		.select("avatar_url, name")
 		.eq("id", userdata.user?.id);
 
+	console.log();
 	return (
 		<div className=" flex flex-col gap-6  py-6 max-sm:items-center ">
 			{posts?.map(async (post) => {
 				const url = supabase.storage.from("Posts").getPublicUrl(post.url);
-				const comments = await supabase
-					.from("comments")
-					.select(`*,
-						profiles(name,avatar_url)
-						`)
-					.eq("post_id", post.id);
 
-				const { data: user } = await supabase
-					.from("profiles")
-					.select("*")
-					.eq("id", post.user_id);
-
-				const { data, error } = await supabase
-					.from("post_likes")
-					.select("user_id")
-					.eq("post_id", post.id);
-
-				const hearts = data?.map((d) => d.user_id as string);
+				const hearts = post?.post_likes?.map(
+					(d: { user_id: string; name: string }) => d.user_id as string,
+				);
 				const initialIsheartIconPressed = hearts?.includes(
 					userdata.user?.id as string,
 				);
@@ -54,7 +49,7 @@ export default async function Home() {
 						initialIsheartIconPressed={initialIsheartIconPressed || false}
 						hearts={hearts}
 						user={
-							(user?.[0] as {
+							(post?.profiles as {
 								id: string;
 								first_name: "text";
 								name: string;
@@ -65,11 +60,11 @@ export default async function Home() {
 						url={url.data.publicUrl}
 						title={post.title}
 						key={post.id}
-						visualisations={post.visualisations}
+						visualisations={post.views}
 						place={post.place}
 						id={post.id}
 						initialIsBookMarkIconPressed={initialIsBookMarkIconPressed || false}
-						comments={comments.data || []}
+						comments={post?.comments || []}
 						userdata={userdataprofiles.data?.[0]}
 					/>
 				);

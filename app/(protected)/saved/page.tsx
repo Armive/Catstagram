e8a92ee13a8711bb4b2a13d5fd70405c83c94b55
Juan <jsreyes979@gmail.com>
2024-12-07 13:32:16 -0500

    remove console log

--- app/(protected)/saved/page.tsx
+++ app/(protected)/saved/page.tsx
@@ -3,6 +3,5 @@ import { SavedGallery } from "@/components/saved/SavedGallery";
 
 export default async function SavedPage() {
 	const saved = await getSaved();
-	console.log(saved);
 	return <SavedGallery data={saved} />;
 }
